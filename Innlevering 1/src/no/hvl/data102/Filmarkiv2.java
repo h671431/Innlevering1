4536d627db4674d8c248e28ecf0df9008c6573a7
hannahhofseth <hannahhofseth@158-37-195-209.hvl.no>
2023-01-31 11:25:00 +0100

    Påbegynt oppg.3 (LinearNode og Filmarkiv2)

---
+++ Innlevering 1/src/no/hvl/data102/Filmarkiv2.java
@@ -0,0 +1,13 @@
+package no.hvl.data102;
+
+import no.hvl.data102.adt.FilmarkivADT;
+
+public class Filmarkiv2 implements FilmarkivADT{
+	
+	private int antall;
+	private LinearNode<Film> start;
+	
+
+	
+
+}
